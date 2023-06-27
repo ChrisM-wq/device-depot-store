e292a96ca8d9003c199848612db00105381cec09
“ChrisM-wq” <chrismchardy1@gmail.com>
2023-06-28 01:17:39 +0200

    MongoDB integration with models

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+import mongoose, { model, Schema, models } from "mongoose";
+
+const ProductSchema = new Schema({
+    title: {
+        type:String,
+        required: true
+    },
+    description: String,
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Category'
+    },
+    properties: {type:Object},
+});
+
+export const Product = models?.Product || model('Product', ProductSchema)
