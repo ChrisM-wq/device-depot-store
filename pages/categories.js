db484f21fcb8ba4fd95343d90adc42378b8bea94
Chris McHardy <chrismchardy1@gmail.com>
2023-07-01 11:41:55 +0200

    Mobile reponsive and building out categories

---
+++ pages/categories.js
@@ -0,0 +1,55 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
+
+export default function Categories({ 
+    mainCategories, 
+    categoriesProducts,
+}) {
+    return (
+        <>
+            <Header />
+            <Center>
+                <Title>Categories</Title>
+                {mainCategories.map((category) => (
+                    <div>
+                        <h2>{category.name}</h2>
+                        <div>{categoriesProducts[category._id].map(p => (
+                            <div>
+                                {p.title}
+                            </div>
+                        ))}</div>
+                    </div>
+                ))}
+            </Center>
+            
+        </>
+    )
+}
+
+export async function getServerSideProps() {
+
+    const categories = await Category.find();
+    const mainCategories = categories.filter(c => !c.parent);
+    const categoriesProducts = {};
+
+    for (const mainCat of mainCategories) {
+        const mainCatId = mainCat._id.toString();
+        const childCatIds = categories.filter(c => c?.parent?.toString() === mainCatId).map(c => c._id);
+
+        const categoriesIDs = [mainCatId, ...childCatIds];
+
+        const products = await Product.find({category: categoriesIDs}, null, {limit: 3, sort:{'id': -1}})
+        categoriesProducts[mainCatId] = products;
+    }
+
+    return {
+        props: {
+            mainCategories: JSON.parse(JSON.stringify(mainCategories)),
+            categoriesProducts: JSON.parse(JSON.stringify(categoriesProducts)),
+        },
+    };
+
+};
